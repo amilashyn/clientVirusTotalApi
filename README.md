0f191c79695b3bbb3d761e844f6c01ff1d08bbad
amilashyn <amilashyn@gmail.com>
2015-10-14 09:28:24 +0200

    change code and instructions, still limit for 16 per minute

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # clientVirusTotalApi
-repository for client VirusTotal public API 2.0 implementation in Scala
+
+#From VirusTotal documentation:
+#'The chosen format for the VirusTotal Public API v2.0 is HTTP POST requests with JSON object responses and it is limited to at most 4 requests of any nature in any given 1 minute time frame.'
+#As a result from this program you able to request only 16 resources for now (I plane to change it:) ).
+
+This is a repository for client VirusTotal public API 2.0 implementation in Scala
+
+
+For use it:
+1. You need for key (was registred in API)
+2. Generate jar by Intellij Idea or Sbt-assembly
+
+Examples of using:
+- run script in Linux terminal:
+script.sh 'keyApi' 'scan|report' url1 url2 url..
+
+script.sh:
+#!/bin/sh
+java -jar ./projectscala.jar "$@"
--- projectClientVirusTotalApi/src/main/scala/com/example/Hello.scala
+++ projectClientVirusTotalApi/src/main/scala/com/example/Hello.scala
@@ -5,7 +5,7 @@ import scalaj.http.Http
 
 object Hello {
 
-  def response(apiKey: String, name: String, url: String): String = {
+  def responseFromApi(apiKey: String, name: String, url: String): String = {
     name match {
       case "scan" => Http("https://www.virustotal.com/vtapi/v2/url/scan").postForm(Seq("url" -> url, "apikey" -> apiKey)).asString.body
       case "report" => Http("https://www.virustotal.com/vtapi/v2/url/report").postForm(Seq("resource" -> url, "apikey" -> apiKey)).asString.body
@@ -13,49 +13,52 @@ object Hello {
   }
 
   def statusScan(json: JsValue) = {
-    if ((json \ "response_code").as[Int] == 1) println("url '" + (json \ "resource").as[String] + "' was send to VirusTotal Public API v2.0")
+    if ((json \ "response_code").as[Int] == 1) println("Resource '" + (json \ "resource").as[String] + " was send to VirusTotal Public API v2.0")
   }
 
   def statusReport(json: JsValue) = {
+    val resource = (json \ "resource").as[String]
+
     if ((json \ "positives").as[Int] != 0) {
-      println((json \ "positives").as[Int])
-      println((json \ "total").as[Int])
-      println((json \ "resource").as[String])
+      val detected = (json \ "positives").as[Int]
+      val amountScanners = (json \ "total").as[Int]
+
+      println("****\nDetected " + detected + " warnings from " + amountScanners + " URLs Scanners for resource: " + resource)
 
       val check = (json \ "scans").as[JsObject]
 
       check.fieldSet.foreach(x =>
-        if ((x._2 \ "detected").as[Boolean] == true) println("Scanner " + x._1 + " report: " + (x._2)))
+        if ((x._2 \ "detected").as[Boolean] == true) println("Report from Scanner \'" + x._1 + "\' is: " + (x._2) + "\n****"))
     }
+
+    else println("Warnings for \'" + resource + "\' is not detected.")
   }
 
   def main(args: Array[String]): Unit = {
 
     val apiKey = args(0)
     val command = args(1)
-    val url = args.drop(2).mkString("\n")
-    val amountUrl = args.length - 2
+    val url = args.drop(2).toSeq.grouped(4)
 
     command match {
-
       case "scan" =>
-        if (amountUrl > 1) {
-          Json.parse(response(apiKey, "scan", url)).as[JsArray].value.foreach(statusScan)
-        }
-        else {
-          val json: JsValue = Json.parse(response(apiKey, "scan", url))
-          statusScan(json)
-        }
+        url.foreach(
+          x => {
+            if (x.size > 1) Json.parse(responseFromApi(apiKey, "scan", x.mkString("\n"))).as[JsArray].value.foreach(statusScan)
+            else
+              statusScan(Json.parse(responseFromApi(apiKey, "scan", x.mkString)))
+          }
+        )
 
       case "report" =>
-        if (amountUrl > 1) {
-          Json.parse(response(apiKey, "report", url)).as[JsArray].value.foreach(statusReport)
-        }
-        else {
-          val json: JsValue = Json.parse(response(apiKey, "report", url))
+        url.foreach(
+          x => {
+            if (x.size > 1) Json.parse(responseFromApi(apiKey, "report", x.mkString("\n"))).as[JsArray].value.foreach(statusReport)
+            else
+              statusReport(Json.parse(responseFromApi(apiKey, "report", x.mkString)))
+          }
+        )
 
-          statusReport(json)
-        }
     }
 
   }
